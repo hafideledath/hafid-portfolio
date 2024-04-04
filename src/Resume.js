e21dc9884b638d832370bb390d90c869a4117679
Hafid Eledath <hafideledath@gmail.com>
2024-04-04 11:22:46 +0400

    updated resume

--- src/Resume.js
+++ src/Resume.js
@@ -5,125 +5,104 @@ function Resume() {
     <div className="Resume">
         <h2>Skills</h2>
 
-        <h3>Fluent in 7 relevant languages</h3>
-        <p>Fluent in the three web-dev languages (HTML, CSS and JavaScript [and Node.js]), Java, C, C#, and Python, all highly versatile languages</p>
+        <h3>Fluent in Python, Java, C, C#, JavaScript, HTML, CSS</h3>
+        <p>Fluent in the three web-dev languages (HTML, CSS and JavaScript), Java, C, C#, and Python</p>
 
-        <h3>Experienced with front-end libraries</h3>
-        <p>Experienced with React, a front-end JavaScript library</p>
+        <h3>Experienced with React and Next.js</h3>
+        <p>Has experience with the front-end JavaScript libraries React and Next.js</p>
 
-        <h3>Experienced with front-end frameworks</h3>
-        <p>Experienced with front-end frameworks such as React Native, Bootstrap and Tailwind.css</p>
+        <h3>Experienced with Bootstrap and Tailwind CSS</h3>
+        <p>Has experience with the front-end frameworks Bootstrap and Tailwind CSS</p>
 
-        <h3>Experienced with back-end frameworks</h3>
-        <p>Experienced with Flask and Streamlit, back-end Python frameworks for production and prototyping, respectively</p>
+        <h3>Experienced with Flask and Streamlit</h3>
+        <p>Has experience with back-end Python frameworks Flask and Streamlit for production and quick prototyping, respectively</p>
 
-        <h3>Skilled with BaaS tools</h3>
-        <p>Skilled with Google Firebase, to manage users and NoSQL databases</p>
+        <h3>Skilled with Google Firebase</h3>
+        <p>Is skilled with Google Firebase, to manage accounts using NoSQL databases</p>
 
-        <h3>Skilled with UI/UX design software</h3>
-        <p>Skilled with Figma, Adobe XD and Canva</p>
+        <h3>Skilled with Figma and Adobe XD</h3>
+        <p>Is skilled with the UI/UX prototyping softwares Figma and Adobe XD</p>
 
-        <h3>Experience with using IDEs efficiently</h3>
-        <p>Experience with offline and/or locally executed coding environments such as VS Code, Jupyter Notebook, and the Colaboratory. Experienced with cloud-based IDEs for collaboration such as GitHub Codespaces and Replit. Experienced with WYSIWYG editors such as Codux and Wix for quick prototyping.</p>
+        <h3>Experienced with GitHub Pages, Python Anywhere, and Netlify</h3>
+        <p>Has experience with using site-hosting services, such as GitHub Pages (general use), PythonAnywhere (Flask apps), Netlify (React apps).</p>
 
-        <h3>Experienced with creating and hosting full-stack websites</h3>
-        <p>Experienced with site-hosting services, such as GitHub pages, Python Anywhere, and Netlify. Experienced with finding and purchasing domains.</p>
-
-        <h3>Skilled with LLMs</h3>
-        <p>Experience with the ChatGPT API for unique and highly dynamic AI-powered applications</p>
+        <h3>Experienced with Squarespace Domains and Hostinger</h3>
+        <p>Has experience with domain management software, including Squarespace Domains and Hostinger</p>
 
 
         <h2>Education</h2>
 
+        <h3>Taken the IB MYP Digital Design Course</h3>
+        <p>Has taken the IB MYP Digital Design course for the past 3 years, with a 3.9/4.0 GPA and three excellent achievement awards.</p>
+
         <h3>Took Harvard’s CS50x with a verified certificate</h3>
-        <p>Completed Harvard's online project-based Compsci course via EDx, with passing grade, gaining valuable full-stack skills</p>
+        <p>Completed Harvard's online project-based Compsci course via EDx, with passing grades</p>
 
         <h3>Took Harvard’s CS50P</h3>
-        <p>Completed Harvard's online project-based Python course extension of CS50, via EDx; developing Python skills</p>
+        <p>Completed Harvard's online project-based Python course extension of CS50 via EDx, with passing grades</p>
 
         <h3>Took Brown Pre-College's course on Set Theory and Boolean Algebra</h3>
-        <p>Completed Brown Pre-College's 2-week course on Set Theory and Boolean Algebra, live and on-campus. Gained highly valuable skills applicable in machine learning</p>
+        <p>Completed Brown Pre-College's 2-week course on Set Theory and Boolean Algebra, live and on-campus, with passing grades</p>
 
         <h3>Took Daniel Walter Scott’s Responsive Web Design course</h3>
-        <p>Completed Daniel Walter Scott's project-based course on Responsive web-design, via Udemy. This was the catalyst for my passion for web-design</p>
+        <p>Completed Daniel Walter Scott's project-based course on Responsive web-design, via Udemy</p>
 
         <h3>Took Shaun Pelling’s course on modern React web-development</h3>
-        <p>Completed Shaun Pelling's course on modern React web-development, via YouTube. This helped with the creation of <i>Speak Up, DIA's</i> website</p>
-
-        <h3>Currently taking Andrej Karpathy’s course on neural networks and backpropagation</h3>
-        <p>Currently taking a highly challenging but interesting course by Andrej Karpathy, on Artificial Intelligence and Neural Networks</p>
+        <p>Completed Shaun Pelling's course on modern React web-development, via YouTube</p>
 
 
         <h2>Experience and Awards</h2> 
 
-        <h3>Head of technology for DIAPAIR</h3>
-        <p>Acting as the current head of technology for DIAPAIR, a social-networking platform. Designed and developed the platform from scratch, including the accounts infrastructure</p>
+        <h3>Won the Sheikh Hamdan Award for Distinguished Students</h3>
+        <p>Won a 30,000 AED grant for distinguished extracurricular performance, including work for DIAPAIR and Tuberculosis fundraising</p>
 
-        <h3>Website developer for DI@TECH</h3>
-        <p>Currently in the PR department of DI@TECH; chiefly responsible for the creation of the DI@TECH website</p>
+        <h3>Head of Technology for DIAPAIR</h3>
+        <p>Designed and developed the platform from scratch, implementing full-stack architecture, using React, Firebase, and Socket.IO</p>
 
-        <h3>Head of technology for Mathletes</h3>
-        <p>Acting as the current head of technology for Mathletes, which involves working with the deputy head to design and create a relevant website</p>
+        <h3>Website Developer for DI@TECH</h3>
+        <p>Designed and developed the DI@TECH website, prioritizing aesthetics and brand identity, using React and EmailJS</p>
 
-        <h3>Head of technology for the Architecture Club</h3>
-        <p>Acting as the current head of technology for the Architecture club, which involves working with the deputy to create a unique and relevant website</p>
+        <h3>Head of Technology for the Architecture Club</h3>
+        <p>Developed a website for the Architecture Club using pure HTML, CSS, and JavaScript</p>
 
-        <h3>Head of teaching for CodeCrafters</h3>
-        <p>Acting as the current head of teaching for the CodeCrafters CCA</p>
+        <h3>Head of Teaching for CodeCrafters</h3>
+        <p>Coordinated with teachers to create weekly lesson plans to teach computer science concepts to middle school students</p>
 
-        <h3>Head of events for the AI Club</h3>
-        <p>Acting as the current head of events for DIA's AI Club</p>
+        <h3>Head of Events for the AI Club</h3>
+        <p>Collaborated with the Head of Public Relations to ideate and organize events hosted by the AI Club</p>
 
-        <h3>Head of technology for the Crochet Club</h3>
-        <p>Currently the head of technology for Crochet Club DIA, responsible for working with the deputy head to create a relevant website</p>
-
-        <h3>Got into the global round of the Uber Global Hackathon</h3>
-        <p>Participated in the team Code Crusaders, which got into the global round of the Uber Global Hackathon– placing top 3.8% of all 2500 competitors</p>
+        <h3>Global Finalist for the Uber Global Hackathon</h3>
+        <p>Got into the global round of the Uber Global Hackathon, placing in the top 3.8% of all 2500 competitors</p>
 
         <h3>Won DI@BYTE 2024</h3>
-        <p>Got first place in the year 9-12 category of DI@BYTE 2024 by implementing the solution for various problems with my partner, under time pressure</p>
+        <p>Placed first in the seniors category of DI@BYTE 2024 by implementing solutions for various LeetCode-style problems with my partner, under time pressure</p>
 
         <h3>Won DI@BYTE 2023</h3>
-        <p>Got first place in the backend category of DI@BYTE 2023 by designing and implementing a financial tracking app with my partner, under time pressure</p>
+        <p>Placed first in the backend category of DI@BYTE 2023 by designing and implementing a financial tracking app with my partner, under time pressure</p>
 
         <h3>Won the DI@TECH 2023 programming competition</h3>
-        <p>Got first place for the DI@TECH 2023 programming competition by implementing the solution to a variety of problems with my team, under time pressure</p>
+        <p>Placed first in the DI@TECH 2023 programming competition by implementing the solution to a variety of LeetCode-style problems with my team, under time pressure</p>
+
+        <h3>Programmed a web-application that helps train users for history bowls</h3>
+        <p>Programmed a web-application called the Historitron 6000, which asks the user random history questions, made using Flask</p>
+
+        <h3>Wrote a dynamic web-based article to raise awareness and funding for Tuberculosis treatment</h3>
+        <p>Wrote and programmed a web-based article with text that updates in real-time, to spread awareness and raise money for tuberculosis treatment in third-world countries</p>
 
         <h3>Developed a website showcasing poetry from student around DIA</h3>
         <p>Conceived, designed, developed and hosted a website for <i>Speak Up, DIA</i> using React, showcasing student-written poetry; set up analytics using Google Analytics</p>
 
-        <h3>Created three unique, themed developer portfolio pages</h3>
-        <p>Conceived, designed, developed and hosted three uniquely themed portfolio pages: one with the theme of puzzles (with Flask and Bootstrap), another with the theme of comic books and one with the theme of Y2K (both with React).</p>
-
-        <h3>Created a unique online resume for my literary skills</h3>
-        <p>Created a unique <a href="https://literature.hafideledath.com">literary resume</a> to stand out in terms of literary abilities.</p>
-
-        <h3>Created a WordPress blog to write about Compsci and English related content</h3>
-        <p>Created a <a href="https://hafideledath.wordpress.com">WordPress blog</a> to act as a platform to write about topics I'm passionate about, especially Compsci and English</p>
-
-        <h3>Developed a full-stack website demonstrating the Dubai Metro</h3>
-        <p>Conceived, designed, developed and hosted a full-stack website, using Flask, demonstrating the capabilities of the Dubai Metro</p>
-
-        <h3>Programmed a full-stack stock-market trading simulator</h3>
-        <p>Programmed a full-stack stock-market trading simulator using Flask, as part of Harvard's CS50x</p>
-
         <h3>Created ASCII UNO</h3>
-        <p>Conceived and programmed a Python-based terminal game in Python, simulating the card game UNO, with vibrant ASCII art representing cards. The game is played against a rudimentary AI and one version of the game involves GPT-3.5 turbo</p>
+        <p>Conceived and programmed a Python-based terminal game in Python, simulating the card game UNO using ASCII art, played against a rudimentary AI</p>
 
         <h3>Programmed Hafidopoly</h3>
-        <p>Conceived and programmed a Java-based Monopoly-inspired game, with a twist: the game is played locally within an IDE, and changes in the game are reflected real-time on a .md file, with beautifully styled ASCII art</p>
+        <p>Developed a text-based clone of Monopoly with an ASCII art interface in Java, using OOP</p>
 
         <h3>Developed a dynamic, multi-purpose Discord bot</h3>
-        <p>Convieved and developed a multi-purpose Discord bot using Node.js, which allows users to create their own commands, using a syntax I developed. It has a variety of functions, such as dynamic RESTful API data retrieval</p>
-
-        <h3>Awarded excellent achievement for MYP Digital Design in years 9 and 10</h3>
-        <p>Awarded excellent achievement for DD, for exceeding expectations in class</p>
+        <p>Convieved and developed a multi-purpose Discord bot using Node.js, which allows users to create their own commands, using a special syntax</p>
 
         <h3>Programmed an AI-powered celebrity simulator</h3>
         <p>Programmed an AI-powered celebrity and historical figure simulator using OpenAI's GPT-3.5 turbo API</p>
-
-        <h3>Created 5 front-end projects as part of Daniel Walter Scott's course</h3>
-        <p>Created a variety of static websites as part of Daniel Walter Scott's web-design course</p>
 
         <h3>Programmed a full-stack Yahoo Search clone</h3>
         <p>Programmed a full-stack clone of the Yahoo Search engine using Flask, complete with real-time weather and time data, and search autocompletion</p>
@@ -132,16 +111,16 @@ function Resume() {
         <p>Conceived, designed and developed a 2D education RPG game, named 'Duck Debugger', using the Unity Game Engine and programmed in C#</p>
 
 
-        <h2>Projects</h2>
-
-        <h3>Portfolio website – hafideledath.com</h3>
-        <p>The website you're on right now</p>
+        <h2>Links / References</h2>
 
         <h3>DIAPAIR website – diapair.org</h3>
         <p>A DIA social networking platform named DIAPAIR</p>
 
         <h3>DI@TECH website – dia-tech.org</h3>
         <p>The website for a DIA-organized hackathon named DI@TECH</p>
+
+        <h3>Historitron 6000 - https://tinyurl.com/historitron</h3>
+        <p>A web-based application that asks the user random history questions</p>
 
         <h3>Speak up, DIA – poetry.hafideledath.com</h3>
         <p>A website showcasing a book I've published, highlighting my interest in poetry</p>
@@ -151,6 +130,9 @@ function Resume() {
 
         <h3>Interactive article demo – tuberculosis.hafideledath.com</h3>
         <p>An interactive article idea, intended to raise money for tuberculosis treatment</p>
+
+        <h3>Duck Debugger – https://hafideledath.itch.io/duck-debugger</h3>
+        <p>An educational RPG game that teaches players to program</p>
 
     </div>
   );
